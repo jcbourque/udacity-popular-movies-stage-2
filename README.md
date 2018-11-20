942700b6f337e9e32cf392fd2fe0ae523d65f1f4
Chad Bourque <chadbourque@Milas-MacBook-Pro.local>
2018-11-20 00:36:45 -0700

    Initial commit with scaffolding to support externalizing an API key

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # udacity-popular-movies-stage-1
+
+This application uses themoviedb.org API, which requires an API key.
+Add a gradle.properties file to the project folder (where this file is located).
+In the gradle.properties file, add the following key=value pair with your API key:
+
+movieDbApiKey=your_key_goes_here
+
+Once that is done, your key will be used to make the calls to themoviedb.org
---
+++ app/src/main/java/com/example/android/popularmoviesstage1/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.android.popularmoviesstage1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.i(TAG, "onCreate: " + BuildConfig.MOVIE_DB_API_KEY);
+    }
+}
